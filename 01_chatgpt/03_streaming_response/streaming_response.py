264f8ea54de5cba24cd78c5d0e4aa79ee07243a3
Bashir Aziz <bashiraziz@gmail.com>
2023-12-18 11:35:01 -0500

    key test

--- 01_chatgpt/03_streaming_response/streaming_response.py
+++ 01_chatgpt/03_streaming_response/streaming_response.py
@@ -1,15 +1,21 @@
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
-import os
+#import os
 load_dotenv(find_dotenv()) # read local .env file
 
-client = OpenAI()
-
-stream = client.chat.completions.create(
-    model="gpt-3.5-turbo-1106",
-    messages=[{"role": "user", "content": "Say this is a test"}],
-    stream=True,
-)
+import os                                                                                                                                                                                                          
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+load_dotenv(Path("./.env"))
 print(os.getenv("OPENAI_API_KEY"))
-for part in stream:
-    print(part.choices[0].delta.content or "")
+
+# client = OpenAI()
+
+# stream = client.chat.completions.create(
+#     model="gpt-3.5-turbo-1106",
+#     messages=[{"role": "user", "content": "Say this is a test"}],
+#     stream=True,
+# )
+print(os.getenv("OPENAI_API_KEY"))
+# for part in stream:
+#     print(part.choices[0].delta.content or "")
