ec9db23444be3ff8110d237b9485c707a7c346e1
Bashir Aziz <bashiraziz@gmail.com>
2023-12-18 11:40:30 -0500

    ey

--- 01_chatgpt/03_streaming_response/streaming_response.py
+++ 01_chatgpt/03_streaming_response/streaming_response.py
@@ -1,21 +1,14 @@
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
-#import os
-load_dotenv(find_dotenv()) # read local .env file
+import os
+print(load_dotenv(find_dotenv())) # read local .env file
 
-import os                                                                                                                                                                                                          
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
-load_dotenv(Path("./.env"))
-print(os.getenv("OPENAI_API_KEY"))
+client = OpenAI()
 
-# client = OpenAI()
-
-# stream = client.chat.completions.create(
-#     model="gpt-3.5-turbo-1106",
-#     messages=[{"role": "user", "content": "Say this is a test"}],
-#     stream=True,
-# )
-print(os.getenv("OPENAI_API_KEY"))
-# for part in stream:
-#     print(part.choices[0].delta.content or "")
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for part in stream:
+    print(part.choices[0].delta.content or "")
--- 01_chatgpt/03_streaming_response/testkey.py
+++ 01_chatgpt/03_streaming_response/testkey.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 load_dotenv(Path("./.env"))
-#print(os.getenv("OPENAI_API_KEY"))
+print(os.getenv("OPENAI_API_KEY"))
