c4bfc4e7d5926921d0ed2c8c8154c5cc56de04d0
Bashir Aziz <bashiraziz@gmail.com>
2023-12-18 11:12:31 -0500

    current pull

---
+++ 01_chatgpt/03_streaming_response/streaming_response.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv()) # read local .env file
+
+client = OpenAI()
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+print(os.getenv("OPENAI_API_KEY"))
+for part in stream:
+    print(part.choices[0].delta.content or "")
